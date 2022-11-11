1797fc75cfc08b1231830e2f7479005adf3ed0d6
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-11-11 09:31:26 +0300

    try_photo_add_except_in_write_msg

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
 import vk_api
-from server import UserInfo
+from server import UserInfo, UserInfoError
 from config import token
 from vk_api.longpoll import VkLongPoll, VkEventType
-from bot_info import Info
-from keyboard import get_start_keyboard, button_search
+from bot_info import Info, start
+from keyboards import get_start_keyboard, button_search
 
 vk = vk_api.VkApi(token=token)
 give = vk.get_api()
 longpoll = VkLongPoll(vk)
 
 
-# Функция написания сообщений, клавиатура = None, потому что клавиатуры может быть и не быть
-def write_message(user_id, text, keyboard=None):
+# функция заглушка, в последующем она будет передавать найденные в VK данные пользователю
+def search_frends(req):
+    return [{
+        ''
+    }]
+
+
+# Функция написания сообщений пользователю
+def write_message(user_id, text, keyboard=None, attachment=None):
     post = {
         'user_id': user_id,
         'message': text,
         'random_id': 0,
-        'keyboard': keyboard
+        'keyboard': keyboard,
+        'attachment': attachment
     }
     vk.method('messages.send', post)
 
@@ -28,22 +36,29 @@ def write_msg():
             if event.to_me:
                 message = event.text.lower()
                 user_id = event.user_id
-                user = UserInfo(user_id)
+                try:
+                    user = UserInfo(user_id)
+                except UserInfoError as e:
+                    write_message(user_id, e)
+                    write_message(user_id, 'Попробуйте обратится через несколько минут.')
+                    continue
                 name = user.get_name()
-                print(user.get_info())
-                if message == 'начать':
+                if message in start:
                     answer = f'Привет, {name}!\n' \
                              f'Для получения дополнительной информации нажмите "Инфо"\n' \
                              f'Для начала поиска нажмите "Начать поиск"'
-                    # В функции передаем кнопки "Инфо", "Начать поиск"
+
                     write_message(user_id, answer, keyboard=get_start_keyboard())
                 elif message == 'начать поиск':
-                    write_message(user_id, 'Отлично, тогда вперед')
+                    write_message(user_id, 'Отлично, тогда вперед!')
+                    # это я тренируюсь передавать фотографии, в этом формате (часть адресной строки) выдаются сразу фото
+                    write_message(user_id, 'Вот ваши фото', attachment='photo808832_457240640,photo677584128_457240276')
+                    # write_message(search_frends(user.get_info()))
 
                 elif message == 'инфо':
                     write_message(user_id, Info.info(), keyboard=button_search())
                 else:
-                    write_message(user_id, 'Я вас не понимаю :(')
+                    write_message(user_id, 'я вас не понимаю')
 
 
 if __name__ == '__main__':
