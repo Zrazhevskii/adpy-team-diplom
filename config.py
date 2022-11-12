8ea96421e14597f5f4faa1923006c955453d65c7
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-11-12 17:20:25 +0300

    connection_local_config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-
-token = 'token группы ВКонтакте'
-TOKEN_VK_USER = "токен пользователя"
+from local_cofig import TOKEN_VK_USER, token
+# token = 'token группы ВКонтакте'
+# TOKEN_VK_USER = "токен пользователя"
 AGE_DELTA = 2
 AGEFROM = 18
 AGETO = 45
