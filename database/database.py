b39cbda75f5be52c95d79feb8443d9d765480ee6
dima <mtt1@list.ru>
2022-11-15 10:25:32 +0300

    привел имя источника базы данных к формату, для удобства адаптации под свои данные

--- database/database.py
+++ database/database.py
@@ -1,41 +1,23 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, unique=True, nullable=False)
-    name = Column(String, nullable=False)
-    link = Column(String(length=28), unique=True, nullable=False)
+from database.models import create_tables
 
 
-class FavoriteList(Base):
-    __tablename__ = 'favorite_list'
+def create_session():
+    dialect = 'postgresql'  # это имя базы данных(mysql, postgresql, mssql, oracle и так далее).
+    driver = ''  # используемый DBAPI. Этот параметр является необязательным. Если его
+                 # не указать будет использоваться драйвер по умолчанию(если он установлен).
+    username = 'postgres'
+    password = '1983'  # данные для получения доступа к базе данных.
+    host = 'localhost'  # расположение сервера базы данных.
+    port = '5432'  # порт для подключения.
+    database = 'bot'  # название базы данных.
 
-    id = Column(Integer, primary_key=True)
-    favorite_id = Column(Integer, nullable=False)
-    name = Column(String(length=40), nullable=False)
-    bdate = Column(String(length=8), nullable=False)
-    link = Column(String(length=28), nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    DSN = f'{dialect + driver}://{username}:{password}@{host}:{port}/{database}'
 
-    user = relationship(User, backref="favorites")
-
-
-class BlackList(Base):
-    __tablename__ = 'black_list'
-
-    id = Column(Integer, primary_key=True)
-    blacklisted_id = Column(Integer, ForeignKey("user.id"), unique=True, nullable=False)
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-
-    user = relationship(User, backref="blacklist")
-
-
-def create_tables(engine):
-    # Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+    engine = create_engine(DSN)
+    create_tables(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
