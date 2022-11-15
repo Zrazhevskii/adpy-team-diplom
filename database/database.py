42d276c1a9a6689b8cd0a2810c139e3a591f7926
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-11-15 10:28:24 +0300

    вынос в отд-ую функ. create_engine, вынос DSN

--- database/database.py
+++ database/database.py
@@ -1,12 +1,17 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import create_tables
+from local_cofig import db_name, password
+
+DSN = f"postgresql://postgres:{password}@localhost:5432/{db_name}"
+
+
+def get_engine(DSN):
+    return create_engine(DSN)
 
 
 def create_session():
-    # DSN = "postgresql://postgres:PASS@localhost:5432/NAME"
-    engine = create_engine(DSN)
-    create_tables(engine)
+    engine = get_engine(DSN)
+    # create_tables(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
